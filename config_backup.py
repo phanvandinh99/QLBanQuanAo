316e72dbf73cf63ae5281f20a8d5ebde14fd1265
PhanVanDinh <vonhanh271@gmail.com>
2025-10-02 20:45:04 +0700

    Clear file Others

--- config_backup.py
+++ config_backup.py
@@ -11,13 +11,13 @@ class Config:
     FLASK_DEBUG = os.getenv('FLASK_DEBUG', 'True').lower() == 'true'
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://root:@localhost/myshop')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/dbname')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # VNPay
     VNPAY_URL = os.getenv('VNPAY_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html')
-    VNPAY_TMN_CODE = os.getenv('VNPAY_TMN_CODE', 'WSGZMU68')
-    VNPAY_HASH_SECRET = os.getenv('VNPAY_HASH_SECRET', 'HM60DQ1QI6AKYMZUXT7Z2M9SHZWL4P0I')
+    VNPAY_TMN_CODE = os.getenv('VNPAY_TMN_CODE', 'YOUR_VNPAY_TMN_CODE')
+    VNPAY_HASH_SECRET = os.getenv('VNPAY_HASH_SECRET', 'YOUR_VNPAY_HASH_SECRET')
     VNPAY_RETURN_URL = os.getenv('VNPAY_RETURN_URL', 'http://localhost:5000/vnpay_return')
     VNPAY_IPN_URL = os.getenv('VNPAY_IPN_URL', 'http://localhost:5000/vnpay_ipn')
 
@@ -26,29 +26,32 @@ class Config:
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True').lower() == 'true'
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False').lower() == 'true'
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'your_email@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'your_app_password')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'your_email@gmail.com')
 
     # Upload
     UPLOADED_PHOTOS_DEST = os.getenv('UPLOADED_PHOTOS_DEST', 'shop/static/images')
+
 
 class DevelopmentConfig(Config):
     """Development configuration"""
     DEBUG = True
     SQLALCHEMY_ECHO = True
 
+
 class ProductionConfig(Config):
     """Production configuration"""
     DEBUG = False
     SQLALCHEMY_ECHO = False
-    # Additional production settings can be added here
+
 
 class TestingConfig(Config):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     WTF_CSRF_ENABLED = False
+
 
 # Configuration mapping
 config = {
@@ -57,6 +60,7 @@ config = {
     'testing': TestingConfig,
     'default': DevelopmentConfig
 }
+
 
 def get_config(config_name=None):
     """Get configuration class based on environment"""
--- fix_product_table.sql
+++
@@ -1,2 +0,0 @@
--- Fix product table by adding missing colors column
-ALTER TABLE product ADD COLUMN colors TEXT NOT NULL DEFAULT "";
